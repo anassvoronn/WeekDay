0a1b6b2ed5ea5808f4f60bbfe32f88669547bed8
anassvoronn <UQzbwQCt7R3uMfz>
2022-08-10 13:22:19 +0300

    WeekDayPrinter

--- src/main/java/org/dz/solution/WeekDayPrinter.java
+++ src/main/java/org/dz/solution/WeekDayPrinter.java
@@ -1,28 +1,27 @@
 package org.dz.solution;
 
 public class WeekDayPrinter {
-    public void print(final int number){
-        if (number >= 7){
+    public void print(final int number) {
+        if (number >= 7) {
             throw new IllegalArgumentException("It's not a day of the week");
         }
-        if (number < 0){
+        if (number < 0) {
             throw new IllegalArgumentException("It's not a day of the week");
         }
-        if (number == 0){
+        if (number == 0) {
             System.out.print("Monday");
-        }else if (number == 1){
+        } else if (number == 1) {
             System.out.print("Tuesday");
-        }else if (number == 2){
+        } else if (number == 2) {
             System.out.print("Wednesday");
-        }else if (number == 3){
+        } else if (number == 3) {
             System.out.print("Thursday");
-        }else if (number == 4){
+        } else if (number == 4) {
             System.out.print("Friday");
-        }else if (number == 5){
+        } else if (number == 5) {
             System.out.print("Day off");
-        }else if (number == 6){
+        } else if (number == 6) {
             System.out.print("Day off");
         }
-
     }
 }
